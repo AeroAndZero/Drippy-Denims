7563e3ff60fac8f35f934fdd2ede9a40683769d7
Ayush Thakur <ayushthakur667@gmail.com>
2022-08-15 13:53:56 +0530

    little formatting

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,11 @@ import React from "react";
 import './Banner.css';
 
 function Banner(){
-    return <div className="BannerImg">
-        <input type="button" value="SHOP NOW" className="bannerBtn"/>
-    </div>
+    return (
+        <div className="BannerImg">
+            <input type="button" value="SHOP NOW" className="bannerBtn"/>
+        </div>
+    )
 }
 
 export default Banner;
